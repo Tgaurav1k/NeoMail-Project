2bc408156e372481a738881d25aef0ddc659f950
Tgaurav1k <tgaurav1k@gmail.com>
2025-07-09 18:02:47 +0530

    fix: resolved vite optimizeDeps and import issues

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import "./index.css";
+
 import {
   RouterProvider,
   createBrowserRouter,
@@ -7,38 +8,41 @@ import {
   Navigate,
   useNavigate,
 } from "react-router-dom";
-import { useSelector } from "react-redux";
 
-import Navbar from "./tempcomponents/Navbar";
-import Sidebar from "./tempcomponents/Sidebar";
-import Inbox from "./tempcomponents/Inbox";
-import Mail from "./tempcomponents/Mail";
-// import Body from "./temp_components/Body";
-import SendEmail from "./temp_components/sendEmail";
-import Login from "./tempcomponents/Login";
-import SignUp from "./tempcomponents/SignUp";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
 import { Toaster } from "react-hot-toast";
-import { useEffect } from React;
+
+// ✅ Using temp_components folder
+import Navbar from "./temp_components/Navbar";
+import Sidebar from "./temp_components/Sidebar";
+import Inbox from "./temp_components/Inbox";
+import Mail from "./temp_components/Mail";
+import SendEmail from "./temp_components/SendEmail";
+import Login from "./temp_components/Login";
+import SignUp from "./temp_components/SignUp";
+
 const Layout = () => {
   const { user } = useSelector((state) => state.app);
   const navigate = useNavigate();
+
   useEffect(() => {
     if (!user) {
       navigate("/login");
     }
   }, [user]);
 
-  if (!user) return null; // don't show layout while redirecting
+  if (!user) return null; // Don't show layout while redirecting
 
   return (
     <>
       <Navbar />
       <div className="flex">
         <Sidebar />
-        <Outlet /> {/* Nested pages like Inbox and Mail */}
+        <Outlet />
       </div>
       {user && (
-        <div className="absolute w-30% bottom-0 right-20 z-10">
+        <div className="absolute w-[30%] bottom-0 right-20 z-10">
           <SendEmail />
         </div>
       )}
@@ -49,7 +53,7 @@ const Layout = () => {
 const appRouter = createBrowserRouter([
   {
     path: "/",
-    element: <Layout />, // ✅ Includes Navbar + Sidebar
+    element: <Layout />,
     children: [
       {
         path: "/",
